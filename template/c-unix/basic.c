5cb9912465224d912837f0f56c069c32a97aaafe
eel3 <eel3@users.noreply.github.com>
2024-05-17 12:48:07 +0900

    Add templates: C application main entry point on Unix-like environment.

---
+++ template/c-unix/basic.c
@@ -0,0 +1,40 @@
+/*
+ * -*- coding: utf-8-unix -*-
+ * vim:fileencoding=utf-8:ff=unix
+ *
+ * FIXME: <edit a description of this file>
+ */
+
+/* ---------------------------------------------------------------------- */
+/*  */
+/* ---------------------------------------------------------------------- */
+
+/* C standard library */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Unix system call and library */
+/* FIXME: include header files. */
+
+/* Third party library */
+/* FIXME: include header files. */
+
+/* User library */
+/* FIXME: include header files. */
+
+/* ---------------------------------------------------------------------- */
+/*  */
+/* ---------------------------------------------------------------------- */
+
+int
+main(int argc, char *argv[])
+{
+	/* FIXME: write a code here. */
+	int i;
+
+	for (i = 0; i < argc; i++) {
+		(void) puts(argv[i]);
+	}
+
+	return EXIT_SUCCESS;
+}
